c44133e55305cd8d437ce36ab425440826550164
Artur Vasilov <artur.vasilov@dz.ru>
2016-09-09 23:58:42 +0300

    fixed tests for build

--- app/src/test/java/ru/arturvasilov/rxloader/RxLoaderTest.java
+++ app/src/test/java/ru/arturvasilov/rxloader/RxLoaderTest.java
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.observers.TestSubscriber;
 import rx.schedulers.Schedulers;
@@ -21,6 +23,10 @@ public class RxLoaderTest {
 
     private LifecycleHandler mLifecycleHandler;
 
+    static {
+        MockUtils.setupTestSchedulers();
+    }
+
     @Before
     public void setUp() throws Exception {
         Context context = Mockito.mock(Context.class);
@@ -32,7 +38,7 @@ public class RxLoaderTest {
     public void testObservableExecuted() throws Exception {
         TestSubscriber<Integer> subscriber = new TestSubscriber<>();
         Observable.just(1, 2, 3)
-                .compose(RxSchedulers.<Integer>async(Schedulers.io(), Schedulers.computation()))
+                .compose(RxSchedulers.<Integer>async(Schedulers.io(), Schedulers.io()))
                 .compose(mLifecycleHandler.<Integer>load(1))
                 .subscribe(subscriber);
 
@@ -43,6 +49,7 @@ public class RxLoaderTest {
     public void testObservableEmpty() throws Exception {
         TestSubscriber<Object> subscriber = new TestSubscriber<>();
         Observable.empty()
+                .compose(RxSchedulers.async())
                 .compose(mLifecycleHandler.load(1))
                 .subscribe(subscriber);
 
